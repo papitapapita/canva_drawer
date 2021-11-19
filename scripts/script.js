c5b0823ccaa2d1c64c82839bd1ac2e41f29e5457
Jonathan <many.d.r.o.2001@hotmail.com>
2021-11-18 23:44:21 -0500

    Correction of the attribute in the tag

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,9 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+
+context.beginPath();
+context.strokeStyle = "red";
+context.moveTo(200,200);
+context.lineTo(400,400);
+context.stroke();
+context.closePath();
