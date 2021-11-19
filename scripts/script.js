c33e72df30d011ff5c71327e7592516886f64526
Jonathan <many.d.r.o.2001@hotmail.com>
2021-11-19 00:51:27 -0500

    Implementation of a piramid without any input

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+let add1, add2;
+add1 = 0;
+add2 = canvas.width;
+
+console.log(canvas.width);
 
 function draw(color, x1, y1, x2, y2)
 {
@@ -11,3 +16,12 @@ function draw(color, x1, y1, x2, y2)
     context.closePath();
 }
 
+while((add1 <= canvas.width) && (add2 >= 0))
+{
+    draw("red", add1, 0, canvas.width, add1);
+    draw("red", 0, add1, add1, canvas.width);
+    draw("red", canvas.width, add1, add2, canvas.height);
+    draw("red", 0, add2, add1, 0);
+    add1 += 10;
+    add2 -= 10;
+}
